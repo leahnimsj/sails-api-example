10d5cf560bfcc89e40ed947a8bb1224d443f3999
Eric Schwartz <eric.schwartz@theironyard.com>
2017-08-29 14:42:38 -0700

    initial commit

---
+++ api/models/User.js
@@ -0,0 +1,28 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    firstName: {
+      type: 'string',
+      required: true
+    },
+    lastName: {
+      type: 'string',
+      required: true
+    },
+    email: {
+      type: 'email',
+      unique: true,
+      required: true
+    }
+
+  },
+  schema: true
+};
